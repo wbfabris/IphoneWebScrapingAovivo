0cd675f5541c83c2c62e9e03ad125615cc13a4de
Walter Beauclair Fabris <wbfabris@hotmail.com>
2024-11-18 12:55:48 -0300

    app01 commit inicial

---
+++ app01.py
@@ -0,0 +1,11 @@
+import requests
+
+def fetch_page(url):   
+    response = requests.get(url)
+    return response.text
+
+# Teste da função
+if __name__ == '__main__':
+    url = 'https://www.mercadolivre.com.br/apple-iphone-16-pro-1-tb-titnio-preto-distribuidor-autorizado/p/MLB1040287851#polycard_client=search-nordic&wid=MLB5054621110&sid=search&searchVariation=MLB1040287851&position=6&search_layout=stack&type=product&tracking_id=92c2ddf6-f70e-475b-b41e-fe2742459774'
+    page_content = fetch_page(url)
+    print(page_content)  # Mostra os primeiros 500 caracteres do HTML
